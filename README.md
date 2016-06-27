7bb2836d307b3df2235dceff3d90c23f9e41a6a2
Asion <luxuexian99@gmail.com>
2016-06-27 23:58:10 +0800

    cobertura config.

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Spring Boot Base to Advanced
 
 Spring Boot Base to Advanced.
 
-[![Build Status](https://travis-ci.org/luxuexian99/spring-boot-base-to-advanced.svg?branch=master)](https://travis-ci.org/luxuexian99/spring-boot-base-to-advanced) 
+[![Build Status](https://travis-ci.org/luxuexian99/spring-boot-base-to-advanced.svg?branch=master)](https://travis-ci.org/luxuexian99/spring-boot-base-to-advanced) [![Coverage Status](https://coveralls.io/repos/github/luxuexian99/spring-boot-base-to-advanced/badge.svg?branch=master)](https://coveralls.io/github/luxuexian99/spring-boot-base-to-advanced?branch=master) 
 
 This projects is used to learning the Spring Boot and it's related technology.
 Like Spring Cloud and so on.
--- spring-boot-intro/src/test/java/org/asion/intro/SpringBootIntroApplicationTest.java
+++ spring-boot-intro/src/test/java/org/asion/intro/SpringBootIntroApplicationTest.java
@@ -28,6 +28,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 )
 public class SpringBootIntroApplicationTest {
 
+    @SuppressWarnings("SpringJavaAutowiredMembersInspection")
     @Autowired
     private TestRestTemplate restTemplate;
 
@@ -50,11 +51,10 @@ public class SpringBootIntroApplicationTest {
 
     @Test
     public void testCss() throws Exception {
-        ResponseEntity<String> entity = new TestRestTemplate()
-                .getForEntity(uri + "/webjars/bootstrap/3.3.6/css/bootstrap.min.css", String.class);
+        ResponseEntity<String> entity =
+                restTemplate.getForEntity(uri + "/webjars/bootstrap/3.3.6/css/bootstrap.min.css", String.class);
         assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(entity.getBody()).contains("body");
-        assertThat(entity.getHeaders().getContentType())
-                .isEqualTo(MediaType.valueOf("text/css"));
+        assertThat(entity.getHeaders().getContentType()).isEqualTo(MediaType.valueOf("text/css"));
     }
 }
